6fd79188b8582e59604b0f651b13e64d52d77ab2
Alex Chen <y.chen@columbia.edu>
2015-03-05 11:54:49 -0500

    NON-PERSISTENT SERVER-SIDE DATA STORAGE

--- app.js
+++ app.js
@@ -2,15 +2,9 @@
 
 var express = require('express');
 var app = express(); 
+var swig = require('swig');
 
-app.get('/', function(req, res) {
-	res.send('Hello World!')
-})
-
-app.get('/news', function(req, res) {
-	res.send('You hav reached /news!')
-})
-
+// set server as listen
 var server = app.listen(3000, function() {
 
 	var host = server.address().address
@@ -19,3 +13,30 @@ var server = app.listen(3000, function() {
 	console.log('server listening at http://%s:%s', host, port)
 
 })
+
+// use swig.renderFile to render html
+app.engine('html', swig.renderFile);
+
+// set default 'view engine' to html
+app.set('view engine', 'html');
+
+// set views path
+app.set('views', __dirname + '/views');
+
+// turn off caching for dev 
+swig.setDefaults( { cache: false });
+
+// pass in some data
+var people = [ {name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+// send rendered files
+app.get('/', function(req, res) {
+	// res.send('Hello World!')
+	res.render('index', {title: 'Hall of Fame', people: people});
+})
+
+
+// app.get('/news', function(req, res) {
+// 	res.send('You hav reached /news!')
+// })
+
